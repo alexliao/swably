a89821fd637edfb0bc238b9a8da44896a80e3f26
alexliao <alex197445@gmail.com>
2013-02-18 11:25:06 +0800

    init with rails stub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swably::Application.config.secret_token = '93131a843267fac4afbf6c8204b7074be5a36f320aaa35be64542b8f1d932ee835637741b330437f55c12099ee6f77eab9be59f5da5ae77d830990c2e0075c89'
