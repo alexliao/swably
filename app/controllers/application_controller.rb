62266505d2bf884d5463b356fb88658c331ecb23
Alex Liao <alex197445@gmai.com>
2014-01-13 15:48:23 +0800

    fix http_refer for sync

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -108,8 +108,8 @@ class ApplicationController < ActionController::Base
 
   def gen_simple_referer(params)
     ret = params[:r] || params[:referer] || request.env["HTTP_REFERER"]
-    ret = "sync_sina" if ret.match('weibo\.')
-    ret = "sync_tencent" if ret.match('t\.qq\.com')
+    ret = "sync_sina" if ret.match('weibo\.') and controller='comments' and action='show'
+    ret = "sync_tencent" if ret.match('t\.qq\.com') and controller='comments' and action='show'
   end
 
   def login_by_user_id
