491dfaae2065f6001bf84e53b2da262b02a78092
leiliu <liulei32@gmail.com>
2014-08-17 00:41:08 -0700

    Add "*.swp" into .gitignore. Add "/tags/public.json" method for accessing all tags.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,6 +13,11 @@ class TagsController < ApplicationController
     api_response @apps.facade, "apps"
   end
 
+  def public
+  @tags = Tag.find(:all, select: "count(a.tag_id) as count, t.*", joins: "t join app_tags a on t.id=a.tag_id", group: "a.tag_id", order: "count desc, a.id desc")
+  api_response @tags.facade(@current_user), "tags"
+  end
+
 private
 end
 
